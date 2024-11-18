baf9b128cfb296a86e049c54667218ef0f8a532f
galih9 <galihbolang07@gmail.com>
2024-11-18 21:44:25 +0700

    add extra offset for every random module called

--- scenes/Level.gd
+++ scenes/Level.gd
@@ -4,25 +4,32 @@ extends Node3D
 var amnt = 30
 var rng = RandomNumberGenerator.new()
 var offset = 6
-
+var offset_each = 10  
+var spawn_counter = 0 
 var initObs = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for n in amnt:
-		spawnModule(n*offset)
-
-
+		spawnModule(n * offset)
 
 func spawnModule(n):
-	if initObs > 5:
-		rng.randomize()
-		var num = rng.randi_range(0,modules.size()-1)
-		var instance = modules[num].instantiate()
-		instance.position.x = n
-		add_child(instance)
-	else:
+	if initObs < 5: 
 		var instance = modules[0].instantiate()
 		instance.position.x = n
 		add_child(instance)
 		initObs += 1
+	else:
+		if spawn_counter % (offset_each + 1) < offset_each:  
+			var instance = modules[0].instantiate()
+			instance.position.x = n
+			add_child(instance)
+		else:
+			rng.randomize()
+			var num = rng.randi_range(0, modules.size() - 1)
+			var instance = modules[num].instantiate()
+			instance.position.x = n
+			print(num)
+			add_child(instance)
+		
+		spawn_counter += 1
